3e0b5b26b11c16ccf40a6c6b618c4be040b350b9
Sin_R0str0 <ahangel2005@gmail.com>
2024-09-15 15:44:22 -0600

    Ejercicio 1. Listas de caracteres

--- Tareas/Programas/Matrices y arreglos/pt_5_list_char/pt_5_list_char_a2.c
+++ Tareas/Programas/Matrices y arreglos/pt_5_list_char/pt_5_list_char_a2.c
@@ -1,24 +1,36 @@
+/*
+Alumnos: Hernández Peña Angel Adrian, Gil de Gaona Jazmín
+Grupo: 1301
+Programación II. Facultad de Estudios Superiores Acatlán
+Fecha de realización: 15/09/24
+Fecha de actualización: 15/09/24
+
+Características del programa: 
+Desde la consola CMD, el programa recibirá parámetros y creará una lista con ellos, luego:
+1. Usar 4 funciones para insertar `D,R,o,9` en cualquier parte de la estructura.
+2. Usar 4 funciones para eliminar cuatro elementos distintos de la estructura. 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "list.h"
 
-/*
-   Print List
-*/
 
 static void print_list (const List *list) {
+	// Imprimir lista
     ListNode *node;
-    int *data, i;
+    char *data;
+	int i;
 
-    fprintf(stdout, "List size is %d\n", list_size(list));
+    fprintf(stdout, "El tamaño de la lista es: %d\n", list_size(list));
 
     i = 0;
     node = list_head(list);
 
     while (1) {
         data = list_data(node);
-        fprintf(stdout, "list.node[%03d]=%03d, %p -> %p \n", i, *data, node, node->next);
+        fprintf(stdout, "list.node[%03d]=%c, %p -> %p \n", i, *data, node, node->next);
 
         i++;
 
@@ -32,23 +44,28 @@ static void print_list (const List *list) {
 }
 
 
-int main (int argc, char **argv) {
-    List list;
+int main (int argc, char **argv){
+    setlocale(LC_ALL,"spanish");
+	List list;
     ListNode *node;
+    char *data, c;
+    int i;
+    
+    if(argc>1){
+    	
+	}
+	else{
+		printf("\nLa lista se creará vacía.");
+		list_init(&list, free);
+	}
 
-    int *data, i;
-
-    // Initialize the linked list
-    list_init(&list, free);
-
-    // Fill the linked list
+    // 1. LLenar la lista
     node = list_head(&list);
-    for (i = 20; i > 10; i--) {
-
-        if ((data = (int *)malloc(sizeof(int))) == NULL)
+    for (c = ' '; c <= '6'; c+=2){
+        if ((data = (char *)malloc(sizeof(char))) == NULL)
             return 1;
 
-        *data = i;
+        *data = c;
 
         if (list_ins_next(&list, NULL, data) != 0)
             return 1;
@@ -56,72 +73,77 @@ int main (int argc, char **argv) {
 
     print_list(&list);
 
-    node = list_head(&list);
-
-    for (i = 0; i < 7; ++i)
+	node = list_head(&list);
+    fprintf(stdout, "\nAgregando A en la cabeza de la lista.\n");
+    if (list_ins_next(&list, NULL, nuevoData('A')) != 0)
+        return 1;
+    print_list(&list);
+    for (i = 1; i < (list_size(&list)-1)/2-1; ++i)
         node = list_next(node);
 
-    data = list_data(node);
-    fprintf(stdout, "\nRemoving a node after the one containing %03d\n", *data);
-
-    if (list_rem_next(&list, node, (void**)&data) != 0)
-        return 1;
-
-    print_list(&list);
-
-    fprintf(stdout, "\nInserting 187 at the tail of the list\n");
-    *data = 187;
-    if (list_ins_next(&list, list_tail(&list), data) != 0)
-        return 1;
-
-    print_list(&list);
-
-    fprintf(stdout, "\nRemoving a node after the first node\n");
-    if (list_rem_next(&list, list_head(&list), (void**)&data) != 0)
+    fprintf(stdout, "\nAgregando W a mitad de la lista.\n");
+    if (list_ins_next(&list, node, nuevoData('W')) != 0)
         return 1;
     print_list(&list);
-
-    fprintf(stdout, "\nRemoving a node at the head of the list\n");
-    if (list_rem_next(&list, NULL, (void**)&data) != 0)
+	
+	node = list_next(node);
+	node = list_next(node);
+	data = list_data(node);
+	
+	fprintf(stdout, "\nAgregando T después del nodo \"%c\".\n", *data);
+    if (list_ins_next(&list, node, nuevoData('T')) != 0)
+        return 1;
+    print_list(&list);
+	
+	node = list_head(&list);
+	for (i = 1; i < 5; ++i)
+        node = list_next(node);
+	
+	data = list_data(node);
+    fprintf(stdout, "\nAgregando M después del nodo \"%c\".\n", *data);
+    if (list_ins_next(&list, node, nuevoData('M')) != 0)
         return 1;
     print_list(&list);    
     
-    fprintf(stdout, "\nInsert 975 at the head of the list\n");
-    *data = 975;
-    if (list_ins_next(&list, NULL, data) != 0)
-        return 1;
-    print_list(&list);    
-
-    fprintf(stdout, "\nIterating and removing the fourth node\n");
-
-    node = list_head(&list);
-    node = list_next(node);
-    node = list_next(node);
-
+    node=list_head(&list);
+    for (i = 0; i < list_size(&list)-2; ++i)
+        node = list_next(node);
+        
+    data=list_data(node);
+    fprintf(stdout, "\nQuitando el nodo de la cola.\n");
     if (list_rem_next(&list, node, (void **)&data) != 0)
         return 1;
-
     print_list(&list);
-
-    fprintf(stdout, "\nInserting 607 after the first node\n");
-    *data = 607;
-    if (list_ins_next(&list, list_head(&list), data) != 0)
+	
+	node=list_head(&list);
+    for (i = 0; i < 3; ++i)
+        node = list_next(node);
+        
+    data=list_data(node);
+    fprintf(stdout, "\nQuitando el nodo siguiente a \"%c\"\n", *data);
+    if (list_rem_next(&list, node, (void **)&data) != 0)
         return 1;
     print_list(&list);
-
-    i = list_is_head(&list, list_head(&list));
-    fprintf(stdout, "\nTesting list_is_head... value=%d (1=OK)\n", i);
-    i = list_is_head(&list, list_tail(&list));
-    fprintf(stdout, "Testing list_is_head... value=%d (1=OK)\n", i);
-    i = list_is_tail(list_tail(&list));
-    fprintf(stdout, "Testing list_is_tail... value=%d (1=OK)\n", i);
-    i = list_is_tail(list_head(&list));
-    fprintf(stdout, "Testing list_is_tail... value=%d (1=OK)\n", i);
     
-
+    for (i; i < 7; ++i)
+        node = list_next(node);
+        
+    data=list_data(node);
+    fprintf(stdout, "\nQuitando el nodo siguiente a \"%c\"\n", *data);
+    if (list_rem_next(&list, node, (void **)&data) != 0)
+        return 1;
+    print_list(&list);
+    
+    data=list_data(list_head(&list));
+    fprintf(stdout, "\nQuitando la cabeza de la lista.\n");
+    if (list_rem_next(&list, NULL, (void **)&data) != 0)
+        return 1;
+    print_list(&list);
+    
     // Destroying the list
-    fprintf(stdout, "\nDestroying the list\n");
+    fprintf(stdout, "\nDestruyendo lista.\n");
     list_destroy(&list);
-
+	
+	system("pause");
     return 0;
 }
