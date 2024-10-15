a901a4a5a2fad79d847d48b43ad09ae8acafe3aa
Sin_R0str0 <ahangel2005@gmail.com>
2024-10-14 20:55:44 -0600

    Actualización Act. 10.

--- Tareas/Programas/Matrices y arreglos/pt_10_queue/pt_10_queue.c
+++ Tareas/Programas/Matrices y arreglos/pt_10_queue/pt_10_queue.c
@@ -1,4 +1,3 @@
-//EJERCICIO
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,18 +9,19 @@
    Print queue
 */
 
-static void print_queue(const Queue *list) {
+static void print_queue(const Queue *queue) {
 	ListNode *node;
+	char data[20];
 	int size, i;
-	char **data;
-	fprintf(stdout, "Queue size is %d\n", size = queue_size(list));
+
+	fprintf(stdout, "Queue size is %d\n", size = queue_size(queue));
 
 	i = 0;
-	node = list_head(list);
+	node = list_head(queue);
 
 	while (i < size) {
-		*data = list_data(node);
-		fprintf(stdout, "queue[%03d]=%s, %p -> %p\n", i, *data, node, node->next);
+		strcpy(data, (char *)list_data(node));
+		fprintf(stdout, "queue[%03d]=%s, %p -> %p\n", i, data, node, node->next);
 		node = list_next(node);
 	 	i++;
 	}
@@ -31,49 +31,52 @@ static void print_queue(const Queue *list) {
 int main(int argc, char const *argv[]) {
 	Queue queue;
 	int i;
-	char **data;
+	char **data, *d;
 
-	// Initialize the queue
+	// Initialize the stack
 	queue_init(&queue, free);
 
-	for (i = 1; i < argc; i++) {
-		if ((data = (char **)malloc(sizeof(char*))) == NULL)
-	    	return 1;
 
-	   	*data = argv[i];
-		printf("\n Dato es: %s", *data);
+	for (i = 1; i < argc; i++) {
+	   	if((data = (char **)malloc(sizeof(char*))) == NULL)
+	   		return -1;
+		if((*data = (char *)malloc(20*sizeof(char))) == NULL)
+	   		return -2;
+			   		   
+		strcpy(*data,argv[i]);
 		if (queue_enqueue(&queue, *data) != 0)
 	    	return 1;
 	}
 	print_queue(&queue);
-
 	fprintf(stdout, "\nPopping 2 elements\n");
+
 	for (i = 0; i < 2; i++) {
-		if(queue_dequeue(&queue, (void **)&data) == 0)
-			free(data);
-		else
-			return 1;
-	}
+        if (queue_dequeue(&queue, (void **)&data) == 0) {
+            free(data);
+    	}else {
+            return 1;
+        }
+    }
+    print_queue(&queue);
+
+	fprintf(stdout, "\nPushing \"Angel\" and \"Jazmin\"\n");
+	if((d = (char *)malloc(20*sizeof(char))) == NULL)
+	   	return -2;
+	strcpy(d,"Angel");
+	if (queue_enqueue(&queue, d) != 0)
+		return 1;
+
+	if((d = (char *)malloc(20*sizeof(char))) == NULL)
+	   	return -2;
+	strcpy(d,"Jazmin");
+	if (queue_enqueue(&queue, d) != 0)
+		return 1;
 	print_queue(&queue);
 
-	fprintf(stdout, "\nPushing \"Hola\" and \"*133\"\n");
-	if ((data = (char **)malloc(sizeof(char *))) == NULL)
-		return 1;
-	*data = "Hola";
-	if (queue_enqueue(&queue, data) != 0)
-		return 1;
-
-	if ((data = (char **)malloc(sizeof(char*))) == NULL)
-		return 1;
-	*data = "*133";
-	if (queue_enqueue(&queue, data) != 0)
-		return 1;
-	print_queue(&queue);
-
-	if ((data = queue_peek(&queue)) != NULL)
-		fprintf(stdout, "\nPeeking at the top element [value]=%s\n", *data);
+	if ((d = queue_peek(&queue)) != NULL)
+		fprintf(stdout, "\nPeeking at the bottom element [value]=%s\n", d);
 	else
-		fprintf(stdout, "\nPeeking at the top element [value]=NULL\n");
+		fprintf(stdout, "\nPeeking at the bottom element [value]=NULL\n");
 	print_queue(&queue);
 
 	fprintf(stdout, "\nPopping all elements\n");
@@ -81,14 +84,14 @@ int main(int argc, char const *argv[]) {
 		if (queue_dequeue(&queue, (void **)&data) == 0)
 			free(data);
 	}
-	if ((data = queue_peek(&queue)) != NULL)
-		fprintf(stdout, "\nPeeking at the top element [value]=%.5lf\n", *data);
+	if ((d = queue_peek(&queue)) != NULL)
+		fprintf(stdout, "\nPeeking at the top element [value]=%s\n", d);
 	else
 		fprintf(stdout, "\nPeeking at the top element [value]=NULL\n");
 	print_queue(&queue);
 
-	fprintf(stdout, "\nDestroying the queue\n");
+	fprintf(stdout, "\nDestroying the stack\n");
 	queue_destroy(&queue);
-	return 0;
 	
+	return 0;
 }
